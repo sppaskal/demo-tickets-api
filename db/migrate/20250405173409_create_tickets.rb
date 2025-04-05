0b6700d5b5253ff7b3870e47fb82e17d7d4dff19
sppaskal <paskalevslavi@gmail.com>
2025-04-05 10:53:08 -0700

    Updating tickets model and migrations
    - Updated tickets model precision and scale parameters
    - Applied migrations and generated schema file + development database

--- db/migrate/20250405173409_create_tickets.rb
+++ db/migrate/20250405173409_create_tickets.rb
@@ -3,7 +3,7 @@ class CreateTickets < ActiveRecord::Migration[8.0]
     create_table :tickets do |t|
       t.string :seat_row
       t.string :seat_number
-      t.decimal :price
+      t.decimal :price, precision: 8, scale: 2
       t.references :user, null: false, foreign_key: true
       t.references :event, null: false, foreign_key: true
 
