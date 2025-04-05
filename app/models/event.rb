8ffec525e2f2608faf5467a78e1ee99e6dac54b1
sppaskal <paskalevslavi@gmail.com>
2025-04-05 10:58:19 -0700

    Adding associations to models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
   belongs_to :organization
+  has_many :tickets
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,8 @@
 class Ticket < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  def seat
+    "Row #{seat_row}, Seat #{seat_number}"
+  end
 end
